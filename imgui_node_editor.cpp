31503f8f370c83357ad51cc04d9619ba26bae517
Gondos <thegondos@protonmail.com>
2022-10-02 20:56:53 +0200

    Fix spurious crashes on startup

--- imgui_node_editor.cpp
+++ imgui_node_editor.cpp
@@ -1124,11 +1124,11 @@ void ed::EditorContext::Begin(const char* id, const ImVec2& size)
     if (!m_IsInitialized)
     {
         // Cycle canvas so it has a change to setup its size before settings are loaded
-        m_Canvas.Begin(id, canvasSize);
-        m_Canvas.End();
-
-        LoadSettings();
-        m_IsInitialized = true;
+        if(m_Canvas.Begin(id, canvasSize)) {
+            m_Canvas.End();
+            m_IsInitialized = true;
+            LoadSettings();
+        }
     }
 
     //ImGui::LogToClipboard();
